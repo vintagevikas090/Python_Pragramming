da34c87da80099c63cff00347d8552d2f229fc2b
vintagevikas090 <127077470+vintagevikas090@users.noreply.github.com>
2023-12-10 15:34:13 +0530

    Delete Basic directory

--- Basic/word_rev.py
+++
@@ -1,6 +0,0 @@
-
-'''Write a Python program that accepts a word from the user and reverses it.'''
-
-word = input("Enter any word: ")
-rev_word = word[::-1]
-print("Reverse of the word given is ", rev_word)
