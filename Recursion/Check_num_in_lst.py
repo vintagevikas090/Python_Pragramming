8c2c848357c0c65e8a5924ef2f47f01abad445fa
Vikas Prajapat <127077470+vintagevikas090@users.noreply.github.com>
2024-09-07 12:05:54 +0530

    Update Check_num_in_lst.py

--- Recursion/Check_num_in_lst.py
+++ Recursion/Check_num_in_lst.py
@@ -9,5 +9,15 @@ def checknum(arr, n):
     smallarray = checknum(arr[1:], n)
     return smallarray
 
+
+def checknum2(arr, n, index = 0):
+    l = len(arr)
+
+    if index == l:
+        return False
+    if arr[index] == n:
+        return True
+    return checknum2(arr,n,index+1)
+
 li = [1,2,3,4,5,6,7]
 print(checknum(li, 4))
