1f00570c267f1796270c259d006468832b4cf3ad
Vikas Prajapat <127077470+vintagevikas090@users.noreply.github.com>
2024-09-09 10:31:10 +0530

    Update Valid Parenthesis String.py

--- String/Valid Parenthesis String.py
+++ String/Valid Parenthesis String.py
@@ -38,10 +38,21 @@ class Solution:
                     stack1.pop()
                 elif stack2: 
                     stack2.pop()
+        (***)
         while stack1 and stack2:
             if stack1.pop() > stack2.pop(): 
                 return False
         return not stack1
+
+
+
+(***)
+at the end, s1 may have (   and s2 may have *
+
+so now the * can onlyyy behave as right para or empty string 
+And
+if the * has to behave as right para, it should have appeared after the left para in string hence its index should be more then left para index 
+So at any point, the index popped from s1 is more, we return false
         
         
         
