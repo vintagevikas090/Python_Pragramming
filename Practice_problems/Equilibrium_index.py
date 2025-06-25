40d65eb5d007adb110cfc688b9a2689e530ca84d
Vikas Prajapat <127077470+vintagevikas090@users.noreply.github.com>
2025-06-25 15:43:58 +0530

    Update Equilibrium_index.py

--- Practice_problems/Equilibrium_index.py
+++ Practice_problems/Equilibrium_index.py
@@ -15,3 +15,17 @@ def eq_index(arr):
             return index
         left_sum = left_sum + arr[index]
         index+=1
+
+
+def equilibrium_index(arr):
+    if len(arr) == 1:
+        return 0
+    left_sum = arr[0]
+    total_sum = sum(arr)
+    for i in range(1, len(arr)):
+        right_sum = total_sum - left_sum - arr[i]
+        if right_sum == left_sum:
+            return i, arr[i]
+        left_sum = left_sum + arr[i]
+        
+    return -1
